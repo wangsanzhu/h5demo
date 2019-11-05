4581b9b27a183a9e68c4a75530d6f78349a4d5f5
kirawang <kirawang@tencent.com>
2019-11-05 13:25:47 +0800

    Updates

---
+++ precache-manifest.56152b19e73388010f157d0f746b4e02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9a1c84aa1767ee11bb1592a6bed93a0",
+    "url": "/h5demo/index.html"
+  },
+  {
+    "revision": "029ef6bc881a5a4a64f0",
+    "url": "/h5demo/static/css/2.65d0fb34.chunk.css"
+  },
+  {
+    "revision": "cbde4131e13274a7446d",
+    "url": "/h5demo/static/css/main.46ee80f5.chunk.css"
+  },
+  {
+    "revision": "029ef6bc881a5a4a64f0",
+    "url": "/h5demo/static/js/2.1c91de1d.chunk.js"
+  },
+  {
+    "revision": "cbde4131e13274a7446d",
+    "url": "/h5demo/static/js/main.fa7e33d6.chunk.js"
+  },
+  {
+    "revision": "0528c0c11cf235eb0e48",
+    "url": "/h5demo/static/js/runtime-main.1124eed8.js"
+  }
+]);
